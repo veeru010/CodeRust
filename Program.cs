8783bcbbabf73e95943f49ac6621e9c575361328
Veerash Ayyagari <veeru.010@gmail.com>
2018-01-21 12:03:25 -0800

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeRust
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
